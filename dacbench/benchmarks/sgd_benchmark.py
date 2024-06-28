28b81d1a8483f88423c9d6fdcf9323515353a9a5
gierle <leon.gieringer@gmx.de>
2024-06-28 19:41:11 +0200

    [WIP] make SGD bench cuda ready and extend state space

--- dacbench/benchmarks/sgd_benchmark.py
+++ dacbench/benchmarks/sgd_benchmark.py
@@ -93,9 +93,9 @@ SGD_DEFAULTS = objdict(
         ],
         "torch_hub_model": (False, False, False),
         "optimizer_params": {
-            "weight_decay": 10.978902603194243,
-            "eps": 1.2346464628039852e-10,
-            "betas": (0.9994264825468422, 0.9866804882743139),
+            # "weight_decay": 10.978902603194243,
+            # "eps": 1.2346464628039852e-10,
+            # "betas": (0.9994264825468422, 0.9866804882743139),
         },
         "cutoff": 1e2,
         "loss_function": __default_loss_function,
--- dacbench/envs/sgd.py
+++ dacbench/envs/sgd.py
@@ -7,6 +7,7 @@ import random
 import numpy as np
 import torch
 
+from collections import deque
 from dacbench import AbstractMADACEnv
 from dacbench.envs.env_utils import sgd_utils
 from dacbench.envs.env_utils.sgd_utils import random_torchvision_loader
@@ -17,8 +18,11 @@ def set_seeds(seed: int) -> None:
     np.random.seed(seed)
     random.seed(seed)
 
+
 class SGD_Momentum(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-3, momentum=0, dampening=0, weight_decay=0, nesterov=False):
+    def __init__(
+        self, params, lr=1e-3, momentum=0, dampening=0, weight_decay=0, nesterov=False
+    ):
         if lr <= 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if momentum < 0.0:
@@ -27,20 +31,44 @@ class SGD_Momentum(torch.optim.Optimizer):
             raise NotImplementedError("Dampening not implemented.")
         if weight_decay != 0:
             raise NotImplementedError("Weight decay not implemented.")
-        
-        defaults = dict(lr=lr, momentum=momentum, dampening=dampening, 
-                        weight_decay=weight_decay, nesterov=nesterov)
+
+        defaults = dict(
+            lr=lr,
+            momentum=momentum,
+            dampening=dampening,
+            weight_decay=weight_decay,
+            nesterov=nesterov,
+        )
+        self.momentum = momentum
+        self.state = dict()
+        for group in self.param_groups:
+            for p in group["params"]:
+                self.state[p] = dict(
+                    vel=torch.zeros_like(p.data), 
+                    grad=torch.zeros_like(p.data),
+                    weights=torch.zeros_like(p.data)
+                )
         super(SGD_Momentum, self).__init__(params, defaults)
 
-    def step(self,):
-        for group in self.param_groups: 
-            for p in group['params']: 
-                if p not in self.state: 
-                    self.state[p] = dict(mom=torch.zeros_like(p.data)) 
-                mom = self.state[p]['mom'] 
-                mom = self.momentum * mom - group['lr'] * p.grad.data 
-                p.data.add_(mom)
+    def step(
+        self,
+    ):
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p not in self.state:
+                    self.state[p] = dict(
+                        vel=torch.zeros_like(p.data), 
+                        grad=torch.zeros_like(p.data),
+                        weights=torch.zeros_like(p.data)
+                    )
 
+                self.state[p]["grad"] = p.grad.data
+                self.state[p]["data"] = p.data
+                velocity = self.state[p]["vel"]
+                velocity.mul_(self.momentum)
+                velocity.add_(group["lr"] * p.grad.data)
+
+                p.data -= velocity
 
 
 def _optimizer_action(
@@ -82,44 +110,11 @@ def test(
             output = model(d_data)
             _, preds = output.max(dim=1)
             test_losses.append(loss_function(output, d_target))
-            test_accuracies.append(torch.sum(preds == target) / len(target))
+            test_accuracies.append(torch.sum(preds == d_target) / len(d_target))
             i += 1
             if i >= nmb_sets:
                 break
-    return torch.cat(test_losses), torch.tensor(test_accuracies)
-
-
-def forward_backward(model, loss_function, loader, device="cpu"):
-    """Do a forward and a backward pass for given `model` for `loss_function`.
-
-    Returns:
-        loss: Mini batch training loss per data point
-    """
-    model.train()
-    (data, target) = next(iter(loader))
-    data, target = data.to(device), target.to(device)
-    output = model(data)
-    loss = loss_function(output, target)
-    loss.mean().backward()
-    return loss.mean().detach()
-
-
-def run_epoch(model, loss_function, loader, optimizer, device="cpu"):
-    """Run a single epoch of training for given `model` with `loss_function`."""
-    last_loss = None
-    running_loss = 0
-    model.train()
-    for data, target in loader:
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = loss_function(output, target)
-        loss.mean().backward()
-        optimizer.step()
-        last_loss = loss
-        running_loss += last_loss.mean().item()
-    return last_loss.mean().detach(), running_loss / len(loader)
-
+    return torch.cat(test_losses).cpu(), torch.tensor(test_accuracies).cpu()
 
 class SGDEnv(AbstractMADACEnv):
     """The SGD DAC Environment implements the problem of dynamically configuring
@@ -170,6 +165,9 @@ class SGDEnv(AbstractMADACEnv):
         self.fraction_of_dataset = config.get("fraction_of_dataset")
         self.train_validation_ratio = config.get("train_validation_ratio")
 
+        self.lr_history = deque(torch.ones(5) * math.log10(self.initial_learning_rate))
+        self.predictions = deque(torch.zeros(2))
+
         self.rng = np.random.RandomState(self.seed)
 
     def step(self, action: float):
@@ -183,12 +181,16 @@ class SGDEnv(AbstractMADACEnv):
         log_learning_rate = action
         self.learning_rate = 10**log_learning_rate
 
+        # Update action history
+        self.lr_history.pop()
+        self.lr_history.appendleft(log_learning_rate)
+
         self.optimizer = _optimizer_action(
             self.optimizer, self.learning_rate, self.use_momentum
         )
 
         if self.epoch_mode:
-            self.loss, self.average_loss = run_epoch(
+            self.train_loss, self.average_loss = self.run_epoch(
                 self.model,
                 self.loss_function,
                 self.train_loader,
@@ -202,17 +204,17 @@ class SGDEnv(AbstractMADACEnv):
                 self.train_loader,
                 self.device,
             ]
-            self.optimizer.step()
             self.optimizer.zero_grad()
-            self.loss = forward_backward(*train_args)
+            self.train_loss, self.train_accuracy = self.forward_backward(*train_args)
+            self.optimizer.step()
 
         crashed = (
-            not torch.isfinite(self.loss).any()
+            not torch.isfinite(self.train_loss).any()
             or not torch.isfinite(
                 torch.nn.utils.parameters_to_vector(self.model.parameters())
             ).any()
         )
-        self.loss = self.loss.numpy().item()
+        self.train_loss = self.train_loss.numpy().item()
 
         if crashed:
             self._done = True
@@ -289,12 +291,14 @@ class SGDEnv(AbstractMADACEnv):
         )
         self.train_loader, self.validation_loader, self.test_loader = loaders
 
+        self.epoch_length = len(self.train_loader)
+
         self.model = sgd_utils.create_model(
             self.config.get("layer_specification"), len(self.datasets[0].classes)
         )
 
         self.learning_rate = self.initial_learning_rate
-        self.optimizer_type = torch.optim.AdamW
+        self.optimizer_type = SGD_Momentum
         self.info = {}
         self._done = False
 
@@ -312,7 +316,7 @@ class SGDEnv(AbstractMADACEnv):
             self.device,
         ]
         losses, train_acc = test(*train_args)
-        self.loss = losses.mean()
+        self.train_loss = losses.mean().cpu()
 
         test_args = [
             self.model,
@@ -334,8 +338,8 @@ class SGDEnv(AbstractMADACEnv):
         ]
         validation_loss, validation_accuracy = test(*val_args)
 
-        self.validation_loss = validation_loss.mean().detach().numpy()
-        self.validation_accuracy = validation_accuracy.mean().detach().numpy()
+        self.validation_loss = validation_loss.mean().detach().cpu().numpy()
+        self.validation_accuracy = validation_accuracy.mean().detach().cpu().numpy()
 
         self.min_validation_loss = None
 
@@ -368,20 +372,46 @@ class SGDEnv(AbstractMADACEnv):
         Returns:
             dict: The current state
         """
-        log_learning_rate = math.log10(self.learning_rate) if self.learning_rate != 0 else math.log10(1e-10)
 
-        state = [
-            self.c_step,
+        remaining_budget = torch.tensor(self.c_step - self.n_steps)
+        log_learning_rate = (
+            torch.log10(self.learning_rate)
+            if self.learning_rate != 0
+            else math.log10(1e-10)
+        )
+        lr_hist_deltas = self.lr_history - log_learning_rate
+        prediction_variance = self.predictions[0].var()
+        prediction_change_variance = (self.predictions[1] - self.predictions[0]).var()
+
+        optimizer_state = self.optimizer.state_dict()
+        norm_grad_layer = torch.ones(len(optimizer_state))
+        norm_vel_layer = torch.ones(len(optimizer_state))
+        norm_data_layer = torch.ones(len(optimizer_state))
+        for i, param in enumerate(optimizer_state.keys()):
+            norm_grad_layer[i] = optimizer_state[param]["grad"].norm(p=2)
+            norm_vel_layer[i] = optimizer_state[param]["vel"].norm(p=2)
+            norm_data_layer[i] = optimizer_state[param]["data"].norm(p=2)
+
+
+        state = torch.cat([
+            remaining_budget,
             log_learning_rate,
-            self.loss,
+            lr_hist_deltas[1:], # first one always 0
+            self.train_loss,
             self.validation_loss,
+            self.train_accuracy,
             self.validation_accuracy,
+            prediction_variance,
+            prediction_change_variance,
+            norm_grad_layer,
+            norm_vel_layer,
+            norm_data_layer,
             self._done,
-        ]
+        ])
         if self.epoch_mode:
             state.append(self.average_loss)
 
-        return torch.tensor(np.array(state))
+        return state
 
     def render(self, mode="human"):
         """Render progress."""
@@ -393,8 +423,48 @@ class SGDEnv(AbstractMADACEnv):
                 f"prev_lr {self.optimizer.param_groups[0]['lr'] if self.n_steps > 0 else None}, "  # noqa: E501
                 f"epoch {epoch}/{epoch_cutoff}, "
                 f"batch {batch}/{len(self.train_loader)}, "
-                f"batch_loss {self.loss}, "
+                f"batch_loss {self.train_loss}, "
                 f"val_loss {self.validation_loss}"
             )
         else:
             raise NotImplementedError
+
+    def forward_backward(self, model, loss_function, loader, device="cpu"):
+        """Do a forward and a backward pass for given `model` for `loss_function`.
+
+        Returns:
+            loss: Mini batch training loss per data point
+        """
+        model.train()
+        (data, target) = next(iter(loader))
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        loss = loss_function(output, target)
+        loss.mean().backward()
+
+        # for comparing current and last predictions
+        self.predictions.pop()
+        self.predictions.appendleft(output)
+
+        accuracy = torch.sum(output == target) / len(target)
+        return loss.mean().detach().cpu(), torch.tensor(accuracy).cpu()
+
+    def run_epoch(self, model, loss_function, loader, optimizer, device="cpu"):
+        """Run a single epoch of training for given `model` with `loss_function`."""
+        last_loss = None
+        running_loss = 0
+        predictions = []
+        model.train()
+        for data, target in loader:
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = loss_function(output, target)
+            loss.mean().backward()
+            optimizer.step()
+            predictions.append(output)
+            last_loss = loss
+            running_loss += last_loss.mean().item()
+        self.predictions.pop()
+        self.predictions.appendleft(predictions.mean())
+        return last_loss.mean().detach().cpu(), running_loss.cpu() / len(loader)
