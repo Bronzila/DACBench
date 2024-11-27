713d48d3d8f3f54135785fe339502a15283b2dcd
gierle <leon.gieringer@gmx.de>
2024-11-27 16:24:19 +0100

    add CIFAR10 default settings

--- dacbench/envs/env_utils/sgd_utils.py
+++ dacbench/envs/env_utils/sgd_utils.py
@@ -20,6 +20,8 @@ DATASETS = {
     "CIFAR10": {
         "transform": transforms.Compose(
             [
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
             ]
